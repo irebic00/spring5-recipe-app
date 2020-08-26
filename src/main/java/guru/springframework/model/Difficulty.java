9b019aefdd67c54445e6869c3eca96dc3bf5dcf5
VrebiC <irebic@outlook.com>
2020-08-26 17:20:17 +0200

    Added Category and Difficulty to model

---
+++ src/main/java/guru/springframework/model/Difficulty.java
@@ -0,0 +1,9 @@
+package guru.springframework.model;
+
+public enum Difficulty {
+    EASY,
+    MODERATE,
+    KIND_OFF_HARD,
+    HARD
+}
+
--- src/main/java/guru/springframework/model/Recipe.java
+++ src/main/java/guru/springframework/model/Recipe.java
@@ -17,14 +17,21 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // TODO: Add later
-    //private Difficulty difficulty
+
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -93,11 +100,43 @@ public class Recipe {
         this.directions = directions;
     }
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
     public Notes getNotes() {
         return notes;
     }
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
