08c164faa2fdbc52847f865de40b3ff45c114935
VrebiC <irebic@outlook.com>
2020-09-02 23:02:40 +0200

    List page for ingredients

---
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -0,0 +1,25 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/recipe/{id}/ingredients")
+    public String listIngredients(@PathVariable String id, Model model) {
+
+        model.addAttribute("recipe", recipeService.findRecipeCommandById(Long.valueOf(id)));
+
+        return "recipe/ingredient/list";
+    }
+}
---
+++ src/test/java/guru/springframework/controllers/IngredientControllerTest.java
@@ -0,0 +1,50 @@
+package guru.springframework.controllers;
+
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+
+public class IngredientControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController controller;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        controller = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void testListIngredients() throws Exception {
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        Mockito.when(recipeService.findRecipeCommandById(Mockito.anyLong())).thenReturn(recipeCommand);
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/ingredients"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("recipe/ingredient/list"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("recipe"));
+
+        //then
+        Mockito.verify(recipeService, Mockito.times(1)).findRecipeCommandById(Mockito.anyLong());
+    }
+
+}
